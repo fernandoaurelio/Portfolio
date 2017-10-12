4f9eea57dd4da4cca6d216f2273e4b8bd05b31d9
Fernando Slivera <fernandoslivera@outlook.com>
2017-10-12 17:50:02 -0300

    Layout Done

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+//Task para sass
+gulp.task('sass', function(){
+	return gulp.src('sass/**/*.sass')
+	.pipe(sass({outputStyle:'compressed'}).on('error',sass.logError))
+	.pipe(gulp.dest('./css'));
+
+});
+
+// Task para watch
+
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.sass',['sass']);
+});
+
+// Task Default
+
+gulp.task('default',['sass','watch']);
